605fd966b38929edd3c31837c047ba4d554c9a7d
Adrian-loup <adrianloup@gmail.com>
2026-05-07 08:57:49 +0200

    test fix 03

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	serverExternalPackages: ["mongoose", "ical.js", "@sparticuz/chromium"],
-
-	webpack: (config) => {
-		config.externals.push("@sparticuz/chromium");
-		return config;
-	},
+	turbopack: {},
 };
 
 export default nextConfig;
