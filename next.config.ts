962a332a016d8ae12a42c673a050af3d0aaaed42
Adrian-loup <adrianloup@gmail.com>
2026-05-07 08:12:03 +0200

    test fix 02

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	serverExternalPackages: ["mongoose", "ical.js", "@sparticuz/chromium"],
+
+	webpack: (config) => {
+		config.externals.push("@sparticuz/chromium");
+		return config;
+	},
 };
 
 export default nextConfig;
